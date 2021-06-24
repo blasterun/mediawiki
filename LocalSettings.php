3fbf10f40e4443538f3e1b9dae7756bbe6d0ae73
Andriy Matchyshyn <amatchyshyn@n-ix.com>
2021-06-24 10:41:40 +0300

    add LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -126,11 +126,11 @@ $wgDiff3 = "/usr/bin/diff3";
 
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
-$wgVectorResponsive = true;
-$wgVectorDefaultSkinVersion = '2';
+$wgDefaultSkin = "vector"
+wfLoadSkin( 'MonoBook' );
 
 // $wgVectorDefaultSkinVersion = '1';
 # End of automatically generated settings.
 # Add more configuration options below.
 
-wfLoadSkin( 'Vector' );
+
